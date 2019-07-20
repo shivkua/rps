3df00ddebb4c89ac7c2d5dd70ea80d89c7bf81f9
Akshay Shivkumar <akshay.nburlington@gmail.com>
2019-07-20 14:40:32 -0400

    commit

---
+++ rockpaperscissors.js
@@ -0,0 +1,46 @@
+function computerPlay(){
+    var cpuchoice = ["ROCK", "PAPER", "SCISSORS"];
+    var choose = Math.floor(Math.random() * 3 + 1);
+    return cpuchoice[choose];
+}
+
+function playerplay() {
+    var cpuselection = computerPlay();
+    var userselection = prompt("Enter ROCK, PAPER, or SCISSORS");
+    switch(userselection){
+        case "ROCK":
+            if(cpuSelection == "ROCK"){
+                return "DRAW";
+            }
+            else if(cpuselection == "PAPER"){
+                return "LOSS";
+            }
+            else{
+                return "WIN"
+            }
+        case "SCISSORS":
+            if(cpuSelection == "ROCK"){
+                return "LOSS";
+            }
+            else if(cpuselection == "PAPER"){
+                return "WIN";
+            }
+            else{
+                return "DRAW"
+            }
+        case "PAPER":
+            if(cpuSelection == "ROCK"){
+                return "WIN";
+            }
+            else if(cpuselection == "PAPER"){
+                return "DRAW";
+            }
+            else{
+                return "LOSS"
+            }
+
+
+
+    }
+
+}
